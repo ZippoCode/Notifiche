b34d3e792efb514f97106778b88974b4487d1a33
Salvatore Prochilo <Portatile Angelo>
2017-02-10 02:19:04 +0100

    Versione 0.0.1

--- app/src/main/java/it/tesi/prochilo/notifiche/ServerManagement.java
+++ app/src/main/java/it/tesi/prochilo/notifiche/ServerManagement.java
@@ -2,56 +2,81 @@ package it.tesi.prochilo.notifiche;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.List;
 
 public class ServerManagement {
 
-    public enum HttpMethod{
+    public enum HttpMethod {
         GET, POST, PUT, DELETE;
     }
+
     private String url;
     private HttpURLConnection mHttpURLConnection;
     private URL mUrl = null;
 
     public ServerManagement(String url) {
         this.url = url;
-        try{
+        try {
             mUrl = new URL(url);
-        }catch (IOException ioe){
+        } catch (IOException ioe) {
 
         }
     }
 
     public List<String> getTopic() throws IOException {
-        try{
+        try {
             mHttpURLConnection = (HttpURLConnection) mUrl.openConnection();
             mHttpURLConnection.setRequestMethod(HttpMethod.GET.name());
+            //DA MODIFICARE
             mHttpURLConnection.addRequestProperty("content_type", "application.json");
             final int httpRespondeCode = mHttpURLConnection.getResponseCode();
             InputStream inputStream;
-            if(true){ //VERIFICA IL NUMERO
+            if (true) { //VERIFICA IL NUMERO
                 inputStream = mHttpURLConnection.getInputStream();
             } else {
                 inputStream = mHttpURLConnection.getErrorStream();
             }
             final String httpResponseMessage = mHttpURLConnection.getResponseMessage();
             //Leggi il contenuto tramite l'inputStream
-        }catch (IOException ioe){
+        } catch (IOException ioe) {
 
-        }finally {
+        } finally {
             mHttpURLConnection.disconnect();
         }
         //ritorna la lista dei topic
         return null;
     }
 
-    public void addTopic(){
+    public void addTopic(String topic) {
+        //DA IMPLEMENTARE CON PUT
+    }
+
+    public void addMoreTopic(String listaTopic) {
+        try {
+            mHttpURLConnection = (HttpURLConnection) mUrl.openConnection();
+            mHttpURLConnection.setRequestMethod(HttpMethod.POST.name());
+            mHttpURLConnection.setDoInput(true);
+            mHttpURLConnection.setDoOutput(true);
+            final OutputStream outputStream = mHttpURLConnection.getOutputStream();
+            outputStream.write(listaTopic.getBytes("UTF-8"));
+            //DA MODIFICARE
+            mHttpURLConnection.addRequestProperty("content_type", "json");
+            final int httpResponse = mHttpURLConnection.getResponseCode();
+            //CONTROLLO RISPOSTA
+            InputStream inputStream = mHttpURLConnection.getInputStream();
+
+        } catch (IOException ioe) {
+
+        } finally {
+            mHttpURLConnection.disconnect();
+        }
 
     }
 
-    public void deleteTopic(){
-
+    public void deleteTopic() {
+        //DA IMPLEMENTARE CON DELETE
     }
 }
